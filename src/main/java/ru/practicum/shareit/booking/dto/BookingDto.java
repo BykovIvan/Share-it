b3f761c19041e500bf2c57c2c0a032fb55c503fc
Ivan <bukov123@yandex.ru>
2022-07-26 17:02:30 +0300

    feat: add class Mapper for Booking and ItemRequest

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * // TODO .
- */
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.item.StatusOfItem;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class BookingDto {
+    private Long bookingId;
+    private LocalDate start;
+    private LocalDate end;
+    private Item item;                      //Вещь
+    private User booker;                    //Пользователь, который осуществляет бронирование
+    private StatusOfItem status;            //статус вещи, должен выставлять пользователь
+    private String review;                  //Отзыв, оставляется после успешного бронирования
 }
---
+++ src/main/java/ru/practicum/shareit/requests/dto/ItemRequestMapper.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit.requests.dto;
+
+import ru.practicum.shareit.requests.ItemRequest;
+
+/**
+ * Класс для преобразования объекса ItemRequest в объект ItemRequestDto для предоставления пользователю и обратно
+ * Class for converting an Booking object to an BookingDto object for presentation to the user and vice versa
+ */
+public class ItemRequestMapper {
+    /**
+     * Метод для преобразования ItemRequest в ItemRequestDto
+     * Method to convert ItemRequest to ItemRequestDto
+     */
+    public static ItemRequestDto toItemRequestDto(ItemRequest itemRequest) {
+        return ItemRequestDto.builder()
+                .id(itemRequest.getId())
+                .description(itemRequest.getDescription())
+                .build();
+
+    }
+
+    /**
+     * Метод для преобразования ItemRequestDto в ItemRequest
+     * Method to convert ItemRequestDto to ItemRequest
+     */
+    public static ItemRequest toItemRequest(ItemRequestDto ItemRequestDto) {
+        return ItemRequest.builder()
+                .id(ItemRequestDto.getId())
+                .description(ItemRequestDto.getDescription())
+                .build();
+
+    }
+}
