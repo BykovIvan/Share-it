198e135c66c2350ce58e7c1b94bfdfa56f183a65
Ivan <bukov123@yandex.ru>
2022-09-03 02:09:42 +0300

    bag get item with booking

--- src/main/java/ru/practicum/shareit/item/CommentMapping.java
+++ src/main/java/ru/practicum/shareit/item/CommentMapping.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item;
 import ru.practicum.shareit.user.User;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class CommentMapping {
     public static CommentDto toCommentDto(Comment comment){
@@ -17,7 +18,7 @@ public class CommentMapping {
     public static Comment toComment(CommentDto commentDto, Item item, User author){
         return Comment.builder()
                 .text(commentDto.getText())
-                .created(Timestamp.valueOf(commentDto.getCreated()))
+                .created(Timestamp.valueOf(LocalDateTime.now()))
                 .item(item)
                 .author(author)
                 .build();
