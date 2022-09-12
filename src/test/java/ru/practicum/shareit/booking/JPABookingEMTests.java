717c0b20094680d85e51fb50bba324bcb921a176
Ivan <bukov123@yandex.ru>
2022-09-13 00:38:09 +0300

    test JPA

---
+++ src/test/java/ru/practicum/shareit/booking/JPABookingEMTests.java
@@ -0,0 +1,62 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.item.ItemRepository;
+import ru.practicum.shareit.item.StatusOfItem;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.UserRepository;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class JPABookingEMTests {
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Autowired
+    private BookingRepository repository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void listOfBookingsIsEmpty() {
+        List<Booking> allBookings = repository.findAll();
+        assertThat(allBookings).isEmpty();
+    }
+
+    @Test
+    public void saveItem() {
+        User user = userRepository.save(User.builder()
+                .name("Ivan")
+                .email("ivan@yandex.ru")
+                .build());
+        Item item = itemRepository.save(Item.builder()
+                .name("Car")
+                .description("Car for you")
+                .available(true)
+                .build());
+        Booking booking = repository.save(Booking.builder()
+                        .start(new Timestamp(System.currentTimeMillis()))
+                        .end(new Timestamp(System.currentTimeMillis()))
+                        .booker(user)
+                        .item(item)
+                        .status(StatusOfItem.WAITING)
+                        .build());
+        Assertions.assertNotNull(booking.getId());
+    }
+
+}
---
+++ src/test/java/ru/practicum/shareit/user/JPAUserEMTests.java
@@ -0,0 +1,46 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class JPAUserEMTests {
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    public void contextLoads() {
+        Assertions.assertNotNull(em);
+    }
+
+    @Test
+    public void saveBootStrappingTest(){
+        User user = new User();
+        user.setName("Ivan");
+        user.setEmail("ivan@yandex.ru");
+
+        Assertions.assertNull(user.getId());
+        em.persist(user);
+        Assertions.assertNotNull(user.getId());
+    }
+
+
+    @Test
+    public void saveTest(){
+        User user = new User();
+        user.setName("Ivan");
+        user.setEmail("ivan@yandex.ru");
+
+        Assertions.assertNull(user.getId());
+        repository.save(user);
+        Assertions.assertNotNull(user.getId());
+    }
+
+}
